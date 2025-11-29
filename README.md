c41b6f0c414ab43392b3523ffcdf72bdc43eb64f
soulbar <w1466019548@gmail.com>
2025-11-29 20:53:24 +0800

    Initial deployment setup for AppleCMS

---
+++ README.md
@@ -0,0 +1,8 @@
+\# AppleCMS Deployment
+
+
+
+这是一个用于部署 AppleCMS 的 PHP 项目模板。
+
+
+
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>🚀 AppleCMS 部署测试 - 成功！</h1>";
+echo "<p>如果看到这个页面，说明 PHP 环境已经正常运行。</p>";
+echo "<p>下一步：上传完整的 AppleCMS 文件即可。</p>";
+
+// 显示 PHP 信息
+phpinfo();
+?>
